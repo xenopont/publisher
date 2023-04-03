5d95244ab466c246182f5d00ec6f4ef55c4accb9
Sergei Kovalenko <smith404@live.com>
2023-04-03 21:18:54 +0200

    unicorn rules

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,11 +1,14 @@
+// eslint-disable-next-line unicorn/prefer-module
 module.exports = {
   env: {
     node: true,
+    es2022: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:unicorn/recommended",
   ],
   ignorePatterns: ["./node_modules/"],
   parser: "@typescript-eslint/parser",
@@ -16,7 +19,7 @@ module.exports = {
     project: "./tsconfig.json",
     createDefaultProgram: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "unicorn"],
   rules: {
     //
   },
--- api/src/app.ts
+++ api/src/app.ts
@@ -2,8 +2,8 @@ import * as http from "node:http";
 
 const port = process.env.port || 3000;
 http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World\n");
   })
   .listen(port);
